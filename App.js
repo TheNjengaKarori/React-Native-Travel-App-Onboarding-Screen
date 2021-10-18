9dc2151bd8af28d717f480a74048342b5fc69792
Njengak11 <njengak11@gmail.com>
2021-10-18 16:39:25 +0300

    initial commit

--- App.js
+++ App.js
@@ -1,21 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from "@react-navigation/stack";
 
-export default function App() {
+// screens
+import {
+  OnBoarding,
+} from "./screens";
+
+// screen for stack & tabs
+const Stack = createStackNavigator();
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <NavigationContainer>
+          <Stack.Navigator>
+              {/* Onboarding screen */}
+              <Stack.Screen name="OnBoarding" component={OnBoarding} options={{ headerShown: false }} />
+          </Stack.Navigator>
+      </NavigationContainer>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default () => {
+  return <App />;
+};
